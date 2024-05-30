ea1acf984bcde9ab039efff1b103c7ec9b238d4b
Gabriel Mocioaca <gabriel.mocioaca@Gabriels-MacBook.local>
2024-05-30 18:39:27 +0100

    spending service reads from app documents

--- lib/services/spendings_service/spendings_service.dart
+++ lib/services/spendings_service/spendings_service.dart
@@ -3,10 +3,12 @@ import 'package:pockety/models/spending.dart';
 import 'package:pockety/services/spendings_service/spendings_service_interface.dart';
 
 class SpendingsService implements SpendingsServiceInterface {
+  final String _spendingsBoxKey = 'spendings';
+
   @override
   Future<List<Spending>> getAllSpendings() async {
-    Box box = Hive.box<List<Spending>>('spendings');
-    List<Spending> spendings = box.get('spendings') ?? [];
+    Box box = await Hive.openBox(_spendingsBoxKey);
+    List<Spending> spendings = box.get(_spendingsBoxKey) ?? [];
     return spendings;
   }
 }
--- lib/ui/widgets/common/spendings_list/spendings_list_model.dart
+++ lib/ui/widgets/common/spendings_list/spendings_list_model.dart
@@ -1,9 +1,9 @@
 import 'package:pockety/app/app.locator.dart';
 import 'package:pockety/models/spending.dart';
-import 'package:pockety/services/spendings_service/spendings_service.dart';
+import 'package:pockety/services/spendings_service/spendings_service_interface.dart';
 import 'package:stacked/stacked.dart';
 
 class SpendingsListModel extends FutureViewModel<List<Spending>> {
   @override
-  Future<List<Spending>> futureToRun() => locator<SpendingsService>().getAllSpendings();
+  Future<List<Spending>> futureToRun() => locator<SpendingsServiceInterface>().getAllSpendings();
 }
